446579f98fd34b369b2c2d56b7cdc9a550ed645a
raghavi-anand <raghavianandkumar@gmail.com>
2023-08-06 07:49:20 +0000

    cmt5

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -1,9 +1,15 @@
-data "aws_ami_ids" "ami" {
-  //most_recent = true
-  name_regex       = "Centos-*"
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
 
-output "ami"{
-    value=data.aws_ami_ids.ami
+output "ami" {
+  value = data.aws_ami.ami
+}
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0dee954b08055e577" ]
 }
