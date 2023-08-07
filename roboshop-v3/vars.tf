d04cb0973a302fc73299c7dc7c52a474cad5d83b
raghavi-anand <raghavianandkumar@gmail.com>
2023-08-07 13:20:51 +0000

    roboshop-v3

---
+++ roboshop-v3/vars.tf
@@ -0,0 +1,52 @@
+variable "security_groups" {
+  default = [ "sg-042e2fb781abad065" ]
+}
+
+variable "zone_id" {
+  default = "Z0777144FBIIX48E8YVZ"
+}
+
+variable "components" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name = "cart"
+      instance_type = "t3.micro"
+    }
+    mongodb = {
+      name = "mongodb"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      instance_type = "t3.micro"
+    }
+    redis = {
+      name = "redis"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t3.micro"
+    }
+    payment = {
+      name = "payment"
+      instance_type = "t3.micro"
+    }
+    shipping = {
+      name = "shipping"
+      instance_type = "t3.micro"
+    }
+    rabbitmq = {
+      name = "rabbitmq"
+      instance_type = "t3.micro"
+    }
+  }
+}
